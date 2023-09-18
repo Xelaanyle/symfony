b5eb4852270a9a1f7cff83a979fdb6aa5d054683
Alexandre <Alechemin@gmail.com>
2023-09-18 14:18:27 +0200

    avancement projet symfony

--- src/Controller/Admin/ProjectController.php
+++ src/Controller/Admin/ProjectController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Project;
 use App\Form\ProjectType;
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/project')]
+#[Route('/admin/project')]
 class ProjectController extends AbstractController
 {
     #[Route('/', name: 'app_project_index', methods: ['GET'])]
